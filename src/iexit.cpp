62c94072109b14d52e7b1d6bf69b9719280f9cdd
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2023-03-23 16:52:43 -0400

    [irods/irods#6136] iexit: avoid removing auth file for service account (unless forced)

--- src/iexit.cpp
+++ src/iexit.cpp
@@ -1,8 +1,10 @@
-/*** Copyright (c), The Regents of the University of California            ***
- *** For more information please refer to files in the COPYRIGHT directory ***/
-#include "rods.h"
+#include "irods_exception.hpp"
+#include "irods_service_account.hpp"
 #include "parseCommandLine.h"
 #include "rcMisc.h"
+#include "rods.h"
+
+#include <cstdio>
 
 void usage( char *prog );
 
@@ -11,12 +13,9 @@ main( int argc, char **argv ) {
 
     signal( SIGPIPE, SIG_IGN );
 
-    int status, ix;
     rodsArguments_t myRodsArgs;
-    rodsEnv myEnv;
-    char *envFile;
 
-    status = parseCmdLineOpt( argc, argv, "Vvh", 0, &myRodsArgs );
+    int status = parseCmdLineOpt(argc, argv, "fVvh", 0, &myRodsArgs);
     if ( status ) {
         printf( "Use -h for help\n" );
         exit( 1 );
@@ -25,24 +24,78 @@ main( int argc, char **argv ) {
         usage( argv[0] );
         exit( 0 );
     }
-    ix = myRodsArgs.optind;
+    const int ix = myRodsArgs.optind;
 
-    status = getRodsEnv( &myEnv );
+    rodsEnv myEnv;
+    getRodsEnv(&myEnv);
+    const char *envFile = getRodsEnvFileName();
+    bool isServiceAccountError = false;
+    bool isServiceAccount = false;
+    try {
+        isServiceAccount = irods::is_service_account();
+    }
+    catch (const irods::exception &e) {
+        isServiceAccountError = true;
+        isServiceAccount = true; // to make if/else blocks neater
+        std::fprintf(stderr,
+                     "WARNING: Could not determine whether %s is running as service account "
+                     "due to thrown exception: %s\n",
+                     argv[0],
+                     e.client_display_what());
+    }
 
-    envFile = getRodsEnvFileName();
-    status = unlink( envFile );
-    if ( myRodsArgs.verbose == True ) {
-        printf( "Deleting (if it exists) session envFile:%s\n", envFile );
-        printf( "unlink status = %d\n", status );
+    if (myRodsArgs.verbose == True) {
+        std::printf("Deleting (if it exists) session envFile: %s\n", envFile);
+    }
+    status = unlink(envFile);
+    if (myRodsArgs.verbose == True) {
+        std::printf("unlink status [%d]\n", status);
     }
 
     if ( ix < argc ) {
         if ( strcmp( argv[ix], "full" ) == 0 ) {
-            if ( myRodsArgs.verbose == True ) {
-                obfRmPw( 0 );
+            if (myRodsArgs.verbose == True && !isServiceAccount) {
+                std::printf("Deleting (if it exists) session auth file\n");
+            }
+            if (myRodsArgs.force == True) {
+                if (isServiceAccount) {
+                    if (isServiceAccountError) {
+                        std::printf("WARNING: Cannot determine if %s is running as service account, "
+                                    "but deleting auth file anyway.\n",
+                                    argv[0]);
+                    }
+                    else {
+                        std::printf("WARNING: %s appears to be running as service account, "
+                                    "but deleting auth file anyway.\n",
+                                    argv[0]);
+                    }
+                }
+                // do not prompt
+                status = obfRmPw(1);
+                if (myRodsArgs.verbose == True) {
+                    std::printf("unlink status [%d]\n", status);
+                }
+            }
+            else if (isServiceAccount) {
+                if (isServiceAccountError) {
+                    std::printf("WARNING: Cannot determine if %s is running as service account, "
+                                "Skipping auth file deletion (pass -f to force).\n",
+                                argv[0]);
+                }
+                else {
+                    std::printf("WARNING: %s appears to be running as service account. "
+                                "Skipping auth file deletion (pass -f to force).\n",
+                                argv[0]);
+                }
+            }
+            else if (myRodsArgs.verbose == True) {
+                // prompt
+                status = obfRmPw(0);
+                std::printf("unlink status [%d]\n", status);
             }
             else {
-                obfRmPw( 1 );
+                // do not prompt
+                obfRmPw(1);
             }
         }
         else {
@@ -55,12 +108,13 @@ main( int argc, char **argv ) {
 
 
 void usage( char *prog ) {
-    printf( "Exits iRODS session (cwd) and optionally removes\n" );
-    printf( "the scrambled password file produced by iinit.\n" );
-    printf( "Usage: %s [-vh] [full]\n", prog );
-    printf( "If 'full' is included the scrambled password is also removed.\n" );
-    printf( " -v  verbose\n" );
-    printf( " -V  very verbose\n" );
-    printf( " -h  this help\n" );
-    printReleaseInfo( "iexit" );
+    std::printf("Exits iRODS session (cwd) and optionally removes\n");
+    std::printf("the scrambled password file produced by iinit.\n");
+    std::printf("Usage: %s [-fvVh] [full]\n", prog);
+    std::printf("If 'full' is included the scrambled password is also removed.\n");
+    std::printf(" -f  force removal of auth file\n");
+    std::printf(" -v  verbose\n");
+    std::printf(" -V  very verbose\n");
+    std::printf(" -h  this help\n");
+    printReleaseInfo("iexit");
 }
